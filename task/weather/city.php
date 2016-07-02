f6235dafe68b6252ca87def2182f1b6b60d97df9
caiyuan <896337651@qq.com>
2016-07-03 00:00:28 +0800

    task3

---
+++ task/weather/city.php
@@ -0,0 +1,9 @@
+<?php
+    $ch = curl_init();
+    $url = "https://api.heweather.com/x3/citylist?search=allchina&key=4a6749034d2c40e3ad40918a23d44676";
+    // 执行HTTP请求
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_SSL_VERIFYPEER , false);
+    $res = curl_exec($ch);
+    echo $res;
+?>
---
+++ task/weather/weather.php
@@ -0,0 +1,11 @@
+ 	<?php
+ 	    $city = $_GET['city'];
+
+    	$ch = curl_init();
+    	$url = "https://api.heweather.com/x3/weather?cityid=$city&key=4a6749034d2c40e3ad40918a23d44676";
+    	// 执行HTTP请求
+    	curl_setopt($ch , CURLOPT_URL , $url);
+    	curl_setopt($ch , CURLOPT_SSL_VERIFYPEER , false);
+    	$res = curl_exec($ch);
+    	echo $res;
+    ?>
